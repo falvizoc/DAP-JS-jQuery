a6b6cc96726249b8b548dae2ea2a0f6aea291185
falvizo <falvizo@bitmovil.mx>
2016-09-23 03:00:32 -0500

    jQ-Clase-01

--- index.js
+++ index.js
@@ -2,14 +2,17 @@ $(function () {
    //Código aquí
    $("#accedeID").click(accedeIDcontenido);
    $("#crearElemento").click(creaElemento);
+   $(".btn").click(esconderBLateral);
  });
+
+function esconderBLateral(){
+   $(".side-nav").sideNav("hide");
+}
 
 function accedeIDcontenido(){
    $("h1#titulo").html("Accediendo elementos");
    $("h3#subtitulo").html("Por id, nombre de clase, atributo");
    $("h6#contenido").html("Accediendo por <strong>Por id</strong>");
-   $("h6#contenido").append($(""));
-
 }
 
 function creaElemento(){
